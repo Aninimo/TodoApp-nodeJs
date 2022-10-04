9cd1632dcadf4763c4a255fd2f812bb53d29ceb5
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-10-04 13:33:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const connectToDb = require("./database/db");
 
 connectToDb();
 const app = express();
-const port = 8000;
+const port = 5000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
